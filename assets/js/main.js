5d3420c22f052fb67af770bda188ac524f5212a8
Halavyryn <andreihalavyryn@gmail.com>
2023-10-06 22:26:47 +0300

    Added menu item underlining

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    links('.header__menu-item')
+})
+
+
+const links = selector => {
+    const linksItems = document.querySelectorAll(selector)
+
+    linksItems.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            linksItems.forEach(link => link.classList.remove('active'))
+            item.classList.add('active')
+        })
+    })
+
+}
